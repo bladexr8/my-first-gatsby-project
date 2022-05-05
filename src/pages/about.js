5a050f4cc9ded09c73c6d96be4efc538e7dd18ea
Stephen Moss <bladexr8@gmail.com>
2022-05-05 19:56:46 +1000

    fix tailwind, markdown file rendering

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import { navigate } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/layouts/Layout'
 
-const About = () => {
+const About = ({ data }) => {
+  
+  const {
+    markdownRemark: { html },
+  } = data;
+  
   const triggerNavigation = () => {
     navigate('/')
   }
+  
   return (
     <Layout>
-      <h1>My About Page</h1>
-      <p>This is a sentence about me.</p>
-      <button
-        onClick={() => triggerNavigation()}>
-        Home Page
-      </button>
+      <div className="max-w-5xl mx-auto py-16 lg:py-24 text-center">
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+        <button
+          className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          onClick={() => triggerNavigation()}>
+          Home Page
+        </button>
+      </div>
     </Layout>
   )
 }
 
 export default About
+
+export const query = graphql`
+  {
+    markdownRemark(frontmatter: { type: { eq: "bio" }
+ }) {
+      html
+    }
+  }
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,44 @@
 import React from "react"
 import GatsbyLink from 'gatsby-link'
+import { graphql } from 'gatsby'
 import Layout from "../components/layouts/Layout"
 
-const Index = () => {
+// resultant data from GraphQL query is passed into 
+// our page component via the data prop
+const Index = ({ data }) => {
+  // use the data contained within the query 
+  const {
+    site: {
+      siteMetadata: { name, role },
+    },
+  } = data;
   
   return (
     <Layout>
-      <h1>My Landing Page</h1>
-      <p>This is my landing page.</p>
-      <GatsbyLink to='/about'>About Me</GatsbyLink>
-      
+      <div className="max-w-5xl mx-auto py-16 lg:py-24">
+        <h1 className="text-4xl md:text-6xl font-bold text-black pb-4">{name}</h1>
+        <p className="mb-4">{role}</p>
+        <GatsbyLink 
+          className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+          to='/about'>About Me</GatsbyLink>
+      </div>
     </Layout>
   )
 }
 
 export default Index
+
+// this is run when Gatsby builds page
+export const query = graphql`
+  {
+    site {
+      siteMetadata {
+        name
+        role
+      }
+    }
+  }
+`
 
 /**
  * button navigation example
