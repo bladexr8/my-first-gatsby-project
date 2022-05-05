b11bc22b880012492dbc46c8fd5783441a43ec2a
Stephen Moss <bladexr8@gmail.com>
2022-05-05 16:28:15 +1000

    revert button styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,6 @@ const About = () => {
       <h1>My About Page</h1>
       <p>This is a sentence about me.</p>
       <button
-        className="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border-2 border-purple-400 text-purple-500 hover:bg-purple-600 hover:text-white"
         onClick={() => triggerNavigation()}>
         Home Page
       </button>
