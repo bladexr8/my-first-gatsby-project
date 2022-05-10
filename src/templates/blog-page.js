f38d19bbed9aa1fc350e149dd6de677ab11f3c15
Stephen Moss <bladexr8@gmail.com>
2022-05-10 13:19:34 +1000

    add blog page template generation

--- src/templates/blog-page.js
+++ src/templates/blog-page.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import Layout from '../components/layouts/Layout'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layouts/Layout";
 import TagList from "../components/blog-posts/TagList"
-
-const BlogPage = () => {
+export default function BlogPage({data}) {
+  const {blogpost: {frontmatter: {date, tags, title},
+    html}} = data
+  const shortDate = date.split("T")[0]
   return (
     <Layout>
-      <div className="max-w-5xl space-y-4 mx-auto py-6 md:py-12 overflow-x-hidden lg:overflow-x-visible">
-        <h1 className="text-4xl font-bold">Blog Title</h1>
+      <div className="max-w-5xl space-y-4 mx-auto 
+       py-6 md:py-12 overflow-x-hidden lg:overflow-x-
+       visible">
+        <h1 className="text-4xl font-
+         bold">{title}</h1>
         <div className="flex items-center space-x-2">
-          <p className="text-lg opacity-50">Date</p>
-          <TagList tags={["ux"]} />
+            <p className="text-lg opacity-
+              50">{shortDate}</p>
+        <TagList tags={tags} />
         </div>
-        <div>
-          Article Body
-        </div>
+        <div className="prose max-w-5xl"
+         dangerouslySetInnerHTML={{__html:html}}/>
       </div>
     </Layout>
   );
 }
-
-export default BlogPage
+export const pageQuery = graphql`
+query($slug: String!) {
+    blogpost: markdownRemark(fields: {slug: {eq:
+      $slug}}) {
+        frontmatter {
+          date
+          title
+          tags
+        }
+        html
+      }
+    }`
